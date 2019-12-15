20e7ab2039a42711566f2d5d6675db79e56d65c9
Peters <petersflorentin@gmail.com>
2019-12-15 13:06:52 +0100

    Fix prefab Specters + fx gain Skill

--- Unity Project/Assets/Scripts/Player/SkillsManagement.cs
+++ Unity Project/Assets/Scripts/Player/SkillsManagement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class SkillsManagement : MonoBehaviour
 {
@@ -58,11 +59,13 @@ public class SkillsManagement : MonoBehaviour
                 rColor = 1;
                 bColor = 0.5f;
                 jumpScript.enabled = true;
+                PopParticle(newSkillParticles, new Color(1,0,0,1));
                 break;
             case "dash":
                 rColor = 1f;
                 bColor = 1f;
                 dashScript.enabled = true;
+                PopParticle(newSkillParticles, new Color(0,0,1,1));
                 break;
             case "j_wallJump":
                 wallJumpScript.enabled = true;
@@ -83,6 +86,7 @@ public class SkillsManagement : MonoBehaviour
 
         //SOUND : Gain Compétence
         characterSprite.color = new Color(rColor, gColor, bColor);
+        StartCoroutine(CancelVibration (Vibrations.PlayVibration("NewSkillGain")));
         skills.Add(skillName);
     }
 
@@ -148,4 +152,10 @@ public class SkillsManagement : MonoBehaviour
         // instantiated.GetComponent<ParticleSystem>().startColor = color;
         Destroy(instantiated, instantiated.GetComponent<ParticleSystem>().main.duration + instantiated.GetComponent<ParticleSystem>().main.startLifetime.constantMax);
     }
+
+    public IEnumerator CancelVibration(float delay)
+	{
+		yield return new WaitForSeconds (delay);
+		GamePad.SetVibration(0,0,0);
+	}
 }
